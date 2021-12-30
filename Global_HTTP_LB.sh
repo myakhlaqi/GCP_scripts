eabd21c0477086a9de993a72edeae505fde40966
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2021-12-30 18:07:53 +0800

    Update Global_HTTP_LB.sh

--- Global_HTTP_LB.sh
+++ Global_HTTP_LB.sh
@@ -3,16 +3,16 @@
 # Task 1: Create a project jumphost instance
 # You will use this instance to perform maintenance for the project.
 # Requirements:
-# * Name the instance Instance name .
-# * Use an f1-micro machine type.
-# * Use the default image type (Debian Linux).
+  # * Name the instance Instance name .
+  # * Use an f1-micro machine type.
+  # * Use the default image type (Debian Linux).
 #======================rules======================
 # 1) Create all resources in the default region or zone, unless otherwise directed.
 # 2) Naming normally uses the format team-resource; for example, an instance could be named nucleus-webserver1.
 # 3) Allocate cost-effective resource sizes. Projects are monitored, and excessive resource use will result in the containing project's termination (and possibly yours), so plan carefully. This is the guidance the monitoring team is willing to share: unless directed, use f1-micro for small Linux VMs, and use n1-standard-1 for Windows or other applications, such as Kubernetes nodes.
 #=================================================
 #check for the default region/zone configration and find the default settings
-gcloud config get-value compute/zone
+  gcloud config get-value compute/zone
 # if it didn't set yet. list all configurations to make sure
 gcloud config list --all | grep "zone"
 
