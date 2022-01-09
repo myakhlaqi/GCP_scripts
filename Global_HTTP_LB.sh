713f00fcf783d07d8d40fcf6931761d2d5fbb7a8
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-01-09 22:51:03 +0800

    Update Global_HTTP_LB.sh

--- Global_HTTP_LB.sh
+++ Global_HTTP_LB.sh
@@ -157,5 +157,5 @@ gcloud compute forwarding-rules create nucleus-http-content-rule \
     --target-http-proxy=nucleus-http-lb-proxy \
     --ports=8080
     #--target-pool nucleus-target-pool1
-   
+    
  
