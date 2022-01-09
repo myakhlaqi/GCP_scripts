b452e62067892f6ce52dff5618375b6c5ac4e224
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-01-09 22:50:42 +0800

    Update Global_HTTP_LB.sh

--- Global_HTTP_LB.sh
+++ Global_HTTP_LB.sh
@@ -93,6 +93,7 @@ sed -i -- 's/nginx/Google Cloud Platform - '"\$HOSTNAME"'/'
 /var/www/html/index.nginx-debian.html
 EOF
 
+
 gcloud compute instance-templates create nucleus-backend-template1 \
   --metadata-from-file startup-script=startup.sh
   #  --network=default \
